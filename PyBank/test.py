1a90a4095587db39f1d0780afc54c411e60f20bb
Kirstan Komajda <73805126+kkirstan@users.noreply.github.com>
2020-12-03 14:39:13 -0500

    fixed data folder location

--- PyBank/test.py
+++ PyBank/test.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-csvpath = os.path.join("..", "PyBank", "budget_data.csv")
+csvpath = os.path.join("..", "PyBank/Resources", "budget_data.csv")
 
 months_list = []
 profit_loss_list = []
