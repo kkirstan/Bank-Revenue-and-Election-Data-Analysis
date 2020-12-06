0a2f10f07fc8cef251183aac8341c1e1a4dd2b0e
Kirstan Komajda <73805126+kkirstan@users.noreply.github.com>
2020-12-06 17:11:52 -0500

    added average change code

--- PyBank/test2.py
+++ PyBank/test2.py
@@ -5,6 +5,7 @@ csvpath = os.path.join("..", "PyBank/Resources", "budget_data.csv")
 
 months_list = []
 profit_loss_list = []
+change_list = []
 
 with open(csvpath, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -21,16 +22,18 @@ with open(csvpath, "r") as csvfile:
     
     for x in profit_loss_list:
         total = total + x
-    
-    
+
+    for x in range(1,len(profit_loss_list)):
+        change_list.append(profit_loss_list[x] - profit_loss_list[x-1])
+        average_change = round(sum(change_list)/len(change_list),2)
+
+    maxincrease = max(change_list)         
+    maxloss = min(change_list)
+
+
     print("Financial Analysis")
     print("----------------------------")
     print("Total Months: " + str(total_months))
     print("Total: $" + str(total))
-    
-    
-    
-   
-    
-     
+    print("Average Change: $" + str(average_change))
     
